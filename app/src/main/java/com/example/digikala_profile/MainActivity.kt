e086efc7bddf6f7ef56a689accb8a0917f62ad0e
Elliot <deadman1383matinboy@gmail.com>
2022-09-19 17:13:38 +0430

    this is my first commit for digijaka ui project

---
+++ app/src/main/java/com/example/digikala_profile/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.digikala_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "This is Digikala UI", Toast.LENGTH_SHORT).show()
+    }
+}
